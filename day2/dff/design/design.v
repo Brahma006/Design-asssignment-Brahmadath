a431686567dbd679d24930b7eba7a327331ef1d4
Brahmadath <brahmadathsheeju1@gmail.com>
2026-06-09 22:41:53 +0530

    Update design.v

--- day2/dff/design/design.v
+++ day2/dff/design/design.v
@@ -1,1 +1,18 @@
+module D_ff(
+    input wire clk,      
+    input wire rst,      
+    input wire d,        
+    output reg q         
+);
+
+   
+    always @(posedge clk ) begin
+        if (rst) begin
+            q <= 1'b0;   
+        end else begin
+            q <= d;      
+        end
+    end
+
+endmodule
 
