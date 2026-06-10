d2a469d297b39c6d325df3cc055c8c67aa61487a
Brahmadath <brahmadathsheeju1@gmail.com>
2026-06-10 21:14:35 +0530

    Add testbench for sr_ff module

--- day2/dff/tb/Dff_tb.v
+++ day2/dff/tb/Dff_tb.v
@@ -1,1 +1,65 @@
+module srff_tb;
+
+reg s_tb;
+reg r_tb;
+reg rst_tb;
+reg clk_tb;
+wire q_tb;
+wire qbar_tb;
+
+sr_ff dut (
+    .s(s_tb),
+    .r(r_tb),
+    .rst(rst_tb),
+    .clk(clk_tb),
+    .q(q_tb),
+    .qbar(qbar_tb)
+);
+
+initial
+begin
+    clk_tb = 0;
+    forever #5 clk_tb = ~clk_tb;
+end
+
+initial
+begin
+    s_tb = 0;
+    r_tb = 0;
+    rst_tb = 1;
+
+    #10;
+    rst_tb = 0;
+
+    s_tb = 1;
+    r_tb = 0;
+    #10;
+
+    s_tb = 0;
+    r_tb = 0;
+    #10;
+
+    s_tb = 0;
+    r_tb = 1;
+    #10;
+
+    s_tb = 0;
+    r_tb = 0;
+    #10;
+
+    s_tb = 1;
+    r_tb = 1;
+    #10;
+
+    $finish;
+end
+
+initial
+begin
+    $monitor("Time=%0t clk=%b rst=%b s=%b r=%b q=%b qbar=%b",
+             $time, clk_tb, rst_tb, s_tb, r_tb, q_tb, qbar_tb);
+end
+
+endmodule
+
 
