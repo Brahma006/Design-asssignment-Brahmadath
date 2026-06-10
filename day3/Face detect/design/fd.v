429259d87aaa072c2cefc2307c3a425def1851e2
Brahmadath <brahmadathsheeju1@gmail.com>
2026-06-10 21:25:39 +0530

    Update fd.v

--- day3/Face detect/design/fd.v
+++ day3/Face detect/design/fd.v
@@ -1,1 +1,29 @@
+`timescale 1ns / 1ps
+//////////////////////////////////////////////////////////////////////////////////
+// Company: 
+// Engineer: 
+// 
+// Create Date: 10.06.2026 16:39:57
+// Design Name: 
+// Module Name: facedetect
+// Project Name: 
+// Target Devices: 
+// Tool Versions: 
+// Description: 
+// 
+// Dependencies: 
+// 
+// Revision:
+// Revision 0.01 - File Created
+// Additional Comments:
+// 
+//////////////////////////////////////////////////////////////////////////////////
+
+
+module facedetect(input clk,input [7:0]s_in ,output reg [7:0]s_out);
+always @(posedge clk)
+s_out<=s_in;
+endmodule
+
+
 
